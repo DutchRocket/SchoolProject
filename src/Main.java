77fd12b5c167beb8598f1a0d9d417b1702b59aed
DutchRocket <124450846+DutchRocket@users.noreply.github.com>
2024-11-10 16:15:56 +0100

    opened GUI

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        JFrame f = new JFrame("Game");
+        f.setVisible(true);
+    }
+}
